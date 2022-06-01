36c5cb9d549c66886564dc218130b4df4bb219e8
HungNP <phuochung180896@gmail.com>
2022-06-01 22:00:14 +0700

    Remove unuse models

--- config/lib/job.js
+++ config/lib/job.js
@@ -2,7 +2,6 @@
 
 const CronJob = require('cron').CronJob;
 const clear_export = require('../jobs/clear-export');
-const change_config = require('../jobs/change-config');
 
 var clear_export_job = new CronJob({
   cronTime: '0 0 0 1 *',
@@ -15,18 +14,7 @@ var clear_export_job = new CronJob({
   timeZone: 'Asia/Tokyo'
 });
 
-// Run 0:0:1 every day
-var cron_start_of_day = new CronJob({
-  cronTime: '01 00 00 * * *',
-  onTick: function () {
-    change_config.execute();
-  },
-  start: false,
-  timeZone: 'Asia/Tokyo'
-});
-
 function start() {
   clear_export_job.start();
-  cron_start_of_day.start();
 }
 exports.start = start;
