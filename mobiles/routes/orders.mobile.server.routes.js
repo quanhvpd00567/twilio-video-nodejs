0e5ad86730500ef9effc83fa6d8a1d8c1e6f7fa3
HungNP <phuochung180896@gmail.com>
2022-06-07 21:34:50 +0700

    Submit order api

---
+++ mobiles/routes/orders.mobile.server.routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var controller = require('../controllers/orders.mobile.controller'),
+  policy = require('../policies/core.mobile.policy.js');
+
+module.exports = function (app) {
+  app.route('/api/mobile/orders/submit')
+    .all(policy.tokenAllowed).post(controller.submitOrder);
+};
